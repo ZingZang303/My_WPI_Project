b733aa0c35c3e07a6dd45c1402d9bc6495a476cf
zingzang303 <zingzang303@gmail.com>
2024-07-10 15:11:16 -0400

    7/10/2024 3:11PM

--- 00-intro/intro.js
+++ 00-intro/intro.js
@@ -3,3 +3,28 @@
 //  1. Give every variable/const smallest scope
 //  2. Grab HTML elements by tag and store in const
 //  3. Put all other code inside functions
+
+const colorField = document.getElementById("color-field");
+const fontSizeField = document.getElementById("font-size-field");
+const clickButton = document.getElementById("click-button");
+const text = document.getElementById("text");
+const checkbox = document.getElementById("checkbox");
+const body = document.body;
+
+function buttonPress() {
+    text.innerHTML = "WHY'D YOU CLICK THE BUTTON"
+
+    body.style.backgroundColor = colorField.value;
+    text.style.fontSize = `${fontSizeField.value}px`
+}
+
+function checkboxChange() {
+    let checked = checkbox.checked;
+    console.log(`the checkbox has this state: ${checked}`)
+
+    if(checked) {
+        body.style.outlineStyle = "solid"
+    } else {
+        body.style.outlineStyle = "none"
+    }
+}
